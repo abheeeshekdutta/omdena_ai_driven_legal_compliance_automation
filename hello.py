5c5a7bc66361109ae8a2caec435edf9351ae32bf
Abhishek Dutta <abhishek.dutta1337@gmail.com>
2025-01-24 00:05:51 +0000

    Basic improvements - added functions

--- hello.py
+++ hello.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from omdena-ai-driven-legal-compliance-automation!")
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer import AnonymizerEngine
+# from rich import print
 
+def analyze_text(text):
+    """
+    Analyze the text and return the results
+    """
+    analyzer = AnalyzerEngine()
+    results = analyzer.analyze(text=text,
+                           language='en')
+    return results
+
+def anonymize_text(text):
+    """
+    Anonymize the text and return the anonymized text
+    """
+    analyzer = AnalyzerEngine()
+    results = analyzer.analyze(text=text,
+                           language='en')
+    anonymizer = AnonymizerEngine()
+    anonymized_text = anonymizer.anonymize(text=text,analyzer_results=results)
+    return anonymized_text
 
 if __name__ == "__main__":
-    main()
+
+    text="My name is Abhishek Dutta. I live in Dublin, Ireland. I work as a Data Scientist at Google \
+    and my phone number is +353 87 234 5678."
+
+    print(analyze_text(text))
+    print(anonymize_text(text))
